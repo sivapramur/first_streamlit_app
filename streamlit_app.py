823c166579cf1856945b034d8c36a59d81787d7e
sivapramur <160143389+sivapramur@users.noreply.github.com>
2024-02-15 19:51:20 +0530

    Update streamlit_app.py
    Added Head and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.head('Breakfast Menu')
+streamlit.text('Omega 3 & Bluberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
